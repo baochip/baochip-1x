76b2506be88ae3e637ac0d1121d318a22eaa8f2a
bunnie <bunnie@users.noreply.github.com>
2026-03-21 19:38:35 +0000

    deploy: 54d28551ed463323f4faa6596fa0d90767cb04c1

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-af668345.js',
+                path_to_root + 'searchindex-514e6236.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
